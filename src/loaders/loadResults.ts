b927ed429e6c3fbfbd340e3e28828f113f52722e
Simone Cernuschi <41926336+cersimo97@users.noreply.github.com>
2026-02-27 16:57:51 +0100

    Fix token parsing from localStorage

--- src/loaders/loadResults.ts
+++ src/loaders/loadResults.ts
@@ -2,23 +2,28 @@ import type { Tour } from '../types/Tour'
 
 function loadResults() {
   const results = localStorage.getItem('results')
-  if (results) {
-    const serializedResults: Tour[] = JSON.parse(results).map(
-      (r: {
-        key: string
-        assignedBus: number
-        numPar: number
-        startDate: string
-        endDate: string
-      }) => ({
-        ...r,
-        startDate: new Date(r.startDate),
-        endDate: new Date(r.endDate),
-      })
-    )
-    return { results: serializedResults }
+  try {
+    if (results) {
+      const serializedResults: Tour[] = JSON.parse(results).map(
+        (r: {
+          key: string
+          assignedBus: number
+          numPar: number
+          startDate: string
+          endDate: string
+        }) => ({
+          ...r,
+          startDate: new Date(r.startDate),
+          endDate: new Date(r.endDate),
+        })
+      )
+      return { results: serializedResults }
+    }
+    return { results: [] }
+  } catch {
+    localStorage.removeItem('results')
+    return { results: [] }
   }
-  return { results: [] }
 }
 
 export default loadResults
