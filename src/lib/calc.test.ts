9ecb4ada0c1272aa27642884b51cd77688bbaaab
Simone Cernuschi <cersimo97@gmail.com>
2026-04-17 20:56:29 +0200

    Add testing setup

--- src/lib/calc.test.ts
+++ src/lib/calc.test.ts
@@ -1,10 +1,8 @@
-import { describe, expect, it } from 'vitest'
+import { expect, it } from 'vitest'
 import { assignBuses } from './calc'
 import { input_2, output_2 } from './tours.example'
 
-describe('Assign buses fn()', () => {
-  it.skip('should produce a list of assigned buses', () => {
-    const result = assignBuses(input_2)
-    expect(result).toStrictEqual(output_2)
-  })
+it('produces assigned buses list', () => {
+  const result = assignBuses(input_2)
+  expect(result).toStrictEqual(output_2)
 })
